823c8fd86f2276429d9d8217de89981a7135d81f
Pritesh-0 <priteshagrawal0@gmail.com>
2023-02-20 17:19:45 +0530

    added changes for decreasing speed and stop

--- test.py
+++ test.py
@@ -1,30 +1,56 @@
 import serial
 import keyboard
+import time
+
 '''
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Change the port as per your system
-left_motor_speed = 0
-right_motor_speed = 0
+sob=serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )      
 '''
+left_motor_speed = 64
+right_motor_speed = 192
+
 def update_speeds():
     global left_motor_speed, right_motor_speed
-    if keyboard.is_pressed('w'):
-        left_motor_speed = 127
-        right_motor_speed = 127
-    elif keyboard.is_pressed('a'):
-        left_motor_speed = -127
-        right_motor_speed = 127
-    elif keyboard.is_pressed('s'):
-        left_motor_speed = -127
-        right_motor_speed = -127
-    elif keyboard.is_pressed('d'):
-        left_motor_speed = 127
-        right_motor_speed = -127
-    else:
-        left_motor_speed = 0
-        right_motor_speed = 0
-    print(left_motor_speed, right_motor_speed)
+    if keyboard.is_pressed('w') and left_motor_speed<127:
+        left_motor_speed += 1
+        #right_motor_speed += 1
+    elif keyboard.is_pressed('a') and right_motor_speed<255:
+        #left_motor_speed -= 1
+        right_motor_speed += 1
+    elif keyboard.is_pressed('s') and left_motor_speed>1:
+        left_motor_speed -= 1
+        #right_motor_speed -= 1
+    elif keyboard.is_pressed('d') and right_motor_speed>128:
+        #left_motor_speed += 1
+        right_motor_speed -= 1
+    elif keyboard.is_pressed('x'):
+    	left_motor_speed =64
+    	right_motor_speed =192
 
-while True:
+    #command = chr(left_motor_speed) + chr(right_motor_speed)
+    #ser.write(command.encode())
+    #sob.write(sp.to_bytes(1,'big'))
+    
+
+while True :
     update_speeds()
+    time.sleep(0.1)
 
+    # Decrease motor speeds when keys are released
+    if not keyboard.is_pressed('w') and left_motor_speed > 64:
+        left_motor_speed -= 1
+    if not keyboard.is_pressed('a') and right_motor_speed > 192:
+        right_motor_speed -= 1
+    if not keyboard.is_pressed('s') and left_motor_speed < 64:
+        left_motor_speed += 1
+    if not keyboard.is_pressed('d') and right_motor_speed < 192:
+        right_motor_speed += 1
+        
+    #print(left_motor_speed,right_motor_speed)
 
