f37a19cfe5bb2726ba03b22eb2e4c7448469d7c5
Pritesh-0 <priteshagrawal0@gmail.com>
2023-02-22 09:23:24 +0530

    Stabertooth motos by gamepad

---
+++ st_g.py
@@ -0,0 +1,65 @@
+import serial
+import pygame
+import time
+pygame.init()
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+'''
+sob=serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )      
+'''
+left_motor_speed = 64
+right_motor_speed = 192
+
+def update_speeds():
+    global left_motor_speed, right_motor_speed,b1,b2,b3
+    pygame.event.get()
+    joystick=joysticks[0]
+    b1,b2,b3 = joystick.get_button(0),joystick.get_button(2),joystick.get_button(4)
+    if b3:
+    	if b1 and right_motor_speed<255:
+    		right_motor_speed += 1
+    	elif b2 and right_motor_speed>128:
+    		right_motor_speed -= 1
+    elif not b3:
+    	if b1 and left_motor_speed<127:
+        	left_motor_speed += 1
+    	elif b2 and left_motor_speed>1:
+        	left_motor_speed -= 1
+    '''
+    elif keyboard.is_pressed('x'):
+    	left_motor_speed =64
+    	right_motor_speed =192
+'''
+    #command = chr(left_motor_speed) + chr(right_motor_speed)
+    #cmd=command.encode()
+    #print(cmd)
+    #sob.write(command.encode())
+    #breakpoint()
+    print(left_motor_speed,right_motor_speed)
+    #sob.write(left_motor_speed.to_bytes(1,'big'))
+    #sob.write(right_motor_speed.to_bytes(1,'big'))    
+    #sob.write(bytes(command,encoding='utf-8'))
+
+while True :
+    update_speeds()
+    
+    time.sleep(0.5)
+	
+	
+    if not b1 and left_motor_speed > 64:
+        left_motor_speed -= 1
+    if not b1 and right_motor_speed > 192:
+        right_motor_speed -= 1
+    if not b2 and left_motor_speed < 64:
+        left_motor_speed += 1
+    if not b2 and right_motor_speed < 192:
+        right_motor_speed += 1
+        
+    #print(left_motor_speed,right_motor_speed)
+
